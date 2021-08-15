ef441b0209ae7a655d8e496d02606b3af512b40a
Shaky-Adino <56446258+Shaky-Adino@users.noreply.github.com>
2021-08-15 09:24:32 +0530

    Add files via upload

---
+++ GPSNavigation.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class GPSNavigation {
+	
+	int v;
+	LinkedList<Integer> adj[];
+	
+	GPSNavigation(int v){
+		this.v = v;
+		adj = new LinkedList[v];
+		for(int i=0;i<v;i++)
+			adj[i] = new LinkedList();
+	}
+	
+	public void addEdge(int u, int v) {
+		adj[u].add(v);
+		adj[v].add(u);
+	}
+	
+	public void BFS(int s) {
+		
+		boolean visited[] = new boolean[v];
+		
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		
+		visited[s] = true;
+        queue.add(s);
+        
+        while (queue.size() != 0)
+        {
+            s = queue.poll();
+            
+            System.out.print(s+" ");
+ 
+            for(int v : adj[s]) {
+            	if(!visited[v]) {
+            		visited[v] = true;
+            		queue.add(v);
+            	}
+            }
+        }
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number of vertices :-");
+		int n = sc.nextInt();
+		
+		GPSNavigation g = new GPSNavigation(n);
+		
+		System.out.println("Enter the number of edges :-");
+		int m = sc.nextInt();
+		
+		while(m-- > 0) {
+			System.out.println("Enter the edge (u,v) :-");
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			g.addEdge(u, v);
+		}
+		
+		System.out.println("Enter the starting vertex :-");
+		int s = sc.nextInt();
+		
+		System.out.println("The vertices that can be reached from "+s+" are :-");
+		g.BFS(s);
+	}
+
+}
